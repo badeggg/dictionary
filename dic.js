257517b5b50de648824375e9cbf98958cf632c7b
badeggg <zhaoxuxujc@163.com>
2017-11-07 11:55:38 +0800

    1st commit

---
+++ dic.js
@@ -0,0 +1,12 @@
+const { spawnSync } = require('child_process');
+
+let word = process.argv[2];
+if(!word){
+    console.log('请输入查询词汇');
+    process.exit(0);
+}
+let url = `http://fanyi.baidu.com/sug --data kw=${word} --compressed`;
+
+let curlSubProcess = spawnSync('curl', url.split(' '));
+let result = JSON.parse( curlSubProcess.stdout );
+console.log(result);
